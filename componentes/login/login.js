35e02a636e2934a56d830d7c8a26a45aec1fcec9
vfrancisquini <vlfrancisquini@gmail.com>
2022-04-07 14:28:51 -0300

    criação de login

---
+++ componentes/login/login.js
@@ -0,0 +1,11 @@
+export default function Login(){
+
+    return(
+        <section className={`logonSection`}>
+            <div className="">
+
+            </div>
+
+        </section>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import {useRef, useState} from "react";
+import Login from "../componentes/login/login";
 
-import Botao from '../componentes/botao'
-import Avatar from '../componentes/avatar'
-import {UploadImagem} from "../componentes/uploadImagem";
-
-
-export default function Home() {
-    const [imagem, setImagem] = useState(null);
-   const referenciaInput = useRef(null);
+export default function Index() {
 
   return (
-    <>
-     <h1>olá mundo</h1>
-     <button onClick={() => referenciaInput?.current?.click()}>abrir seletor</button>
-     <div style={{width:200}}>
-         <UploadImagem
-             setImagem={setImagem}
-             imagemPreview={imagem?.preview}
-             aoSetaraReferencia ={(ref) => referenciaInput.current = ref}
-         />
 
-         <Avatar />
-        <Botao texto={'Login'} cor='primaria'  manipularClique= {() => console.log ('botao clicado')  }/>
-
-     </div>
-     </>
+      <Login />
    
   )
 }
